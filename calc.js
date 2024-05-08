99d4dfad1dacbc9ac8b25d12714fd5fe6209d21a
ar5237 <166900945+ar5237@users.noreply.github.com>
2024-05-08 11:38:07 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,72 @@
+
+topDisplay=document.querySelector('#top')
+bottomDisplay=document.querySelector('#bottom')
+
+function operate(operator,num1,num2){
+    if(operator=='+')
+        return num1+num2;
+    else if(operator=='*')
+        return num1*num2;
+    else if(operator=='-')
+        return num1-num2;
+    else if(num2!=0) return num1/num2;
+}
+
+buttons=document.querySelectorAll('button')
+for(let i=0;i<buttons.length;i++){
+    buttons[i].addEventListener('click',function(){
+        let text=buttons[i].textContent;
+        if(buttons[i].id=='clear'){
+            topDisplay.textContent='';
+            bottomDisplay.textContent='';
+        }
+        else if(buttons[i].id=='delete'){
+            if((bottomDisplay.textContent).length!=0){
+                let y=bottomDisplay.textContent;
+                let l=y.length;
+                let z=y.substr(0,(l-1));
+                bottomDisplay.textContent=z;
+            }
+            else{
+                bottomDisplay.textContent=topDisplay.textContent;
+                topDisplay.textContent='';
+                let y=bottomDisplay.textContent;
+                let l=y.length;
+                let z=y.substr(0,(l-1));
+                bottomDisplay.textContent=z;
+            }
+        }
+
+        else if(buttons[i].className=='num'){
+            if(buttons[i].textContent=='.'){
+                let str=bottomDisplay.textContent;
+                let pos=str.search(/\./);
+                if(pos!=-1) bottomDisplay.textContent=str;
+                else bottomDisplay.textContent+=buttons[i].textContent;
+            }
+            else bottomDisplay.textContent+=buttons[i].textContent
+        }
+        else if(buttons[i].className=='operator'){
+            topDisplay.textContent=bottomDisplay.textContent+buttons[i].textContent;
+            bottomDisplay.textContent='';
+        }
+        else if(buttons[i].className=='solve'){
+
+            let x=Number(bottomDisplay.textContent);
+            let y=topDisplay.textContent
+            let len=y.length;
+            let z=Number(y.substr(0,len-1));
+            let op=y.substr(len-1,len);
+            if(op=='/'&&x==0){
+            topDisplay.textContent='SmartAss';
+            bottomDisplay.textContent='';
+            }
+            else{
+                let ans=operate(op,z,x);
+                topDisplay.textContent='';
+                bottomDisplay.textContent=ans;
+            }
+        }
+
+    })
+}
